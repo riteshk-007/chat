b0c9d52a2bf6c7b1e1c940a05833bca20110d6e9
riteshk-007 <codeshorts007@gmail.com>
2024-02-21 14:44:20 +0530

    Add socket.io for real-time communication

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from "express";
 import http from "http";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { Server } from "socket.io";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-http.createServer(app).listen(PORT, () => {
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
@@ -18,4 +21,11 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+// socket.io
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket);
 });
