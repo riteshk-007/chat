b68035a452520f366b09b2f098dca4f3104f2e17
riteshk-007 <codeshorts007@gmail.com>
2024-02-21 13:56:12 +0530

    Add h1 tag and update server.js to serve index.html

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import http from "http";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 
@@ -7,4 +9,11 @@ const PORT = process.env.PORT || 4000;
 
 http.createServer(app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
